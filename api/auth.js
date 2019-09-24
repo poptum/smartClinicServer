68ab0a047dc5d0f1b02d171aff2d7e0db4ebace8
André Muniz <andre.muniz@tcsindustrial.com.br>
2019-09-24 11:37:53 -0300

    Melhoria para autenticação de usuario

--- api/auth.js
+++ api/auth.js
@@ -2,15 +2,32 @@
 const jwt = require("jsonwebtoken");
 const secret = "mysecretsshhh";
 const withAuth = function(req, res, next) {
-  const token = req.body.token;
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+cookies = parseCookies(req)
+console.log(cookies)
+  const token = cookies.token
+
   if (!token) {
+console.log("Unauthorized: No token provided")
     res.status(401).send("Unauthorized: No token provided");
   } else {
     jwt.verify(token, secret, function(err, decoded) {
       if (err) {
+console.log("Unauthorized: Invalid token")
+console.log(err)
         res.status(401).send("Unauthorized: Invalid token");
       } else {
-        req.body.email = decoded.email;
+        req.email = decoded.email;
         next();
       }
     });
--- api/routes/user.js
+++ api/routes/user.js
@@ -7,15 +7,15 @@ const secret = "mysecretsshhh";
 const auth = require('../auth.js');
 module.exports = function(router) {
   router.post("/login", function(req, res) {
-      console.log("login");
+       console.log("login");
       user
         .findOne({
-          where: { email: req.email }
+          where: { email: req.body.email }
         })
         .then(userDb => {
           bcrypt.compare(req.body.password, userDb.password, function(err, decryptPwd) {
             if (!decryptPwd) {
-              res.status(400).send("senha errada");
+              res.status(400).send("Senha incorreta");
             } else {
               //assina token e retorna pro app
               // Issue token
@@ -23,39 +23,31 @@ module.exports = function(router) {
               const token = jwt.sign(payload, secret, {
                 expiresIn: "1h"
               });
-              res.send({token}).sendStatus(200);
+console.log("sucesso")
+                 res.send({token});
             }
           });
         })
         .catch(error => res.status(400).send("email incorreto"));
   });
-
-  router.post("/user", function(req, res) {
-    bcrypt.hash(req.password, saltRounds, function(err, hash) {
+  router.post("/user", auth,function(req, res) {
       console.log("login");
+ console.log("login123");
       return user
-        .create({
-          name: req.name,
-          password: hash,
-          email: req.email,
-          profile: req.profile
-        })
+        .create(req.body)
         .then(todo => res.status(201).send("usuario registrado"))
-        .catch(error => res.status(400).send("error"));
-    });
+        .catch(error => console.log(error));
   });
 
   router.get("/users", auth, function(req, res) {
-    return res.send("users");
-    // Status.find().exec()
-    //   .then(docs => res.status(200)
-    //     .json(docs))
-    //   .catch(err => res.status(500)
-    //     .json({
-    //       message: 'Error finding user',
-    //       error: err
-    //     }))
+console.log(`teste cookie`)
+console.log(req.cookies)
+    return user
+    .findAll()
+    .then(users => res.status(201).send(users))
+    .catch(error => console.log(error),res.status(400).send("error"));
   });
+
   router.get("/user1", function(req, res) {
     return res.send("users");
     // Status.find().exec()
